518b4ed83d781e1c9f22e7fb02200ee15a0f2a8d
human86x <skripachenko.evgeny@gmail.com>
2025-01-07 21:27:11 +0000

    Code improvements - library import

--- sensor_service2.py
+++ sensor_service2.py
@@ -12,7 +12,7 @@ from config_tools.sequencer import execute_sequence
 from config_tools.calibrator import get_correct_EC
 from config_tools.flow_tune import send_command_with_heartbeat, load_flow_rates, load_pump_commands
 from control_libs.arduino import connect_to_arduino, send_command_and_get_response
-from control_libs.EC import get_ec
+from control_libs.electric_conductivity import get_ec
 from control_libs.temperature import read_solution_temperature
 # Establish serial connection
 ser = connect_to_arduino()
