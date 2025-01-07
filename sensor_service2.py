246681a44309752c69c6644b24d680e0690737e7
human86x <skripachenko.evgeny@gmail.com>
2025-01-07 19:15:47 +0000

    Code improvements - library import

--- sensor_service2.py
+++ sensor_service2.py
@@ -85,6 +85,7 @@ def read_ec():
             # Check timestamp
             last_timestamp = datetime.fromisoformat(sensor_data.get("timestamp", "1970-01-01T00:00:00"))
             if datetime.now() - last_timestamp < timedelta(minutes=0.2):
+                print("Last timestamp  {last_timestamp}     New timestamp  {timedelta}    Difference {}")
                 print("EC data is recent; skipping new EC reading.")
                 return sensor_data.get("ec")
 
