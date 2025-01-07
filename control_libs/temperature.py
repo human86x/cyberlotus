6319cacb20f1cc47e6ac9fda456467c478bfbb37
human86x <skripachenko.evgeny@gmail.com>
2025-01-07 21:13:26 +0000

    Code improvements - library import

--- control_libs/temperature.py
+++ control_libs/temperature.py
@@ -1,3 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from control_libs.arduino import connect_to_arduino, send_command_and_get_response
+
+
 def read_solution_temperature():
     response = send_command_and_get_response(b'T')
     if response is not None:
