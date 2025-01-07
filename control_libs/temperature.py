3963bda13c488f1bef765b8e40f528fa71cc3ea3
human86x <skripachenko.evgeny@gmail.com>
2025-01-07 21:16:21 +0000

    Code improvements - library import

--- control_libs/temperature.py
+++ control_libs/temperature.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from arduino import connect_to_arduino, send_command_and_get_response
+from control_libs.arduino import connect_to_arduino, send_command_and_get_response
 
 
 def read_solution_temperature():
