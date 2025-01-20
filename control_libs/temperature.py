865fec1825393a2b8f455a7e4b28208aa06ca3a0
human86x <skripachenko.evgeny@gmail.com>
2025-01-20 13:03:30 +0000

    flask app, EC

--- control_libs/temperature.py
+++ control_libs/temperature.py
@@ -5,7 +5,10 @@ from control_libs.arduino import connect_to_arduino, send_command_and_get_respon
 
 
 def read_solution_temperature(ser):
+    print(f"Trying to obain temperature of the solution through-> {ser}")
     response = send_command_and_get_response(ser, b'T')
+    print(f"Result-> {response}")
+    
     if response is not None:
         try:
             return float(response)
