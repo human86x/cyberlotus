974677b0d3e07c7fdecf6037ed168e6c6eac50df
human86x <skripachenko.evgeny@gmail.com>
2025-01-23 15:55:03 +0000

    flask javascript interface

--- control_libs/temperature.py
+++ control_libs/temperature.py
@@ -1,14 +1,19 @@
 import sys
 import os
+import time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from control_libs.arduino import connect_to_arduino, send_command_and_get_response
-
+from control_libs.system_stats import system_state
 
 def read_solution_temperature(ser):
     print(f"Trying to obain temperature of the solution through-> {ser}")
     response = send_command_and_get_response(ser, b'T')
     print(f"Result-> {response}")
+    #system_state["temperature"] = response
+    #system_state["timestamp"] = response
     
+    system_state["temperature"]["value"] = response
+    system_state["temperature"]["timestamp"] = int(time.time())
     if response is not None:
         try:
             return float(response)
