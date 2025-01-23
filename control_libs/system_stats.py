c6d2eb0b9d7c11ef20c0fc8e49d8da50a882037a
human86x <skripachenko.evgeny@gmail.com>
2025-01-23 14:46:37 +0000

    flask app, EC

---
+++ control_libs/system_stats.py
@@ -0,0 +1,36 @@
+global system_state
+system_state = {
+    "ec": {"value": None, "timestamp": None},
+    "ph": {"value": None, "timestamp": None},
+    "temperature": {"value": None, "timestamp": None},
+    
+    "tank_1": {"level": None, "timestamp": None},
+    "tank_2": {"level": None, "timestamp": None},
+    "tank_3": {"level": None, "timestamp": None},
+    
+    "relay_states": {
+        "relay_a": {"state": None, "timestamp": None},
+        "relay_b": {"state": None, "timestamp": None},
+        "relay_c": {"state": None, "timestamp": None},
+        "relay_d": {"state": None, "timestamp": None},
+        "relay_e": {"state": None, "timestamp": None},
+        "relay_f": {"state": None, "timestamp": None},
+        "relay_g": {"state": None, "timestamp": None},
+        "relay_h": {"state": None, "timestamp": None},
+        "relay_k": {"state": None, "timestamp": None},
+        "relay_l": {"state": None, "timestamp": None},
+        "relay_m": {"state": None, "timestamp": None},
+        "relay_o": {"state": None, "timestamp": None},
+        "relay_p": {"state": None, "timestamp": None},
+        "relay_q": {"state": None, "timestamp": None},
+        "relay_r": {"state": None, "timestamp": None},
+        "relay_s": {"state": None, "timestamp": None},
+        "relay_u": {"state": None, "timestamp": None},
+        "relay_v": {"state": None, "timestamp": None},
+        "relay_w": {"state": None, "timestamp": None},
+        "relay_x": {"state": None, "timestamp": None},
+        "relay_y": {"state": None, "timestamp": None},
+        "relay_z": {"state": None, "timestamp": None}
+    }
+}
+
