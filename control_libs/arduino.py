cede9a2f63cf52d315ad4e34f36b4a73e461fbcf
human86x <skripachenko.evgeny@gmail.com>
2025-01-23 16:49:56 +0000

    flask javascript interface

--- control_libs/arduino.py
+++ control_libs/arduino.py
@@ -83,12 +83,12 @@ def safe_serial_write(pump_name, state, retries=1, timeout=2):
         attempt = 0
         cur_time = int(time.time())
         
-        print(f"system_state[relay_states][pump_{pump_name}][state] = ON")
-        print(f"system_state[relay_states][pump_{pump_name}][timestamp] = {cur_time}")
+        print(f"system_state[relay_states][relay_{pump_name}][state] = ON")
+        print(f"system_state[relay_states][relay_{pump_name}][timestamp] = {cur_time}")
         
         
-        system_state["relay_states"]["pump_" + pump_name]["state"] = "ON"
-        system_state["relay_states"]["pump_" + pump_name]["timestamp"] = int(time.time())
+        system_state["relay_states"]["relay_" + pump_name]["state"] = "ON"
+        system_state["relay_states"]["relay_" + pump_name]["timestamp"] = int(time.time())
        #system_stats["relay_states"]["pump_" + pump_name]["state"] = new_state
        #system_stats["relay_states"]["pump_" + pump_name]["timestamp"] = int(time.time())
         
