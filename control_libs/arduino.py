4108df0610902ebb427fb3570259ea0acfc4ce63
human86x <skripachenko.evgeny@gmail.com>
2025-05-05 13:20:25 +0100

    Front-end system console output

--- control_libs/arduino.py
+++ control_libs/arduino.py
@@ -535,7 +535,7 @@ import serial
 from serial import SerialException
 
 
-ddef send_command_and_get_response(ser, command, retries=1, timeout=2.3):
+def send_command_and_get_response(ser, command, retries=1, timeout=2.3):
     attempt = 0
     global power_ser
     while attempt < retries:
