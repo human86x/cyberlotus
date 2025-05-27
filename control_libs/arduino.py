e9151e25264209b6f4f10692bc1b81a00ebb0505
human86x <skripachenko.evgeny@gmail.com>
2025-05-27 15:06:46 +0100

    Interface and system tuneup

--- control_libs/arduino.py
+++ control_libs/arduino.py
@@ -585,7 +585,7 @@ import serial
 from serial import SerialException
 
 
-def send_command_and_get_response(ser, command, retries=1, timeout=3.3):
+def send_command_and_get_response(ser, command, retries=7, timeout=3.3):
     attempt = 0
     global power_ser
     while attempt < retries:
